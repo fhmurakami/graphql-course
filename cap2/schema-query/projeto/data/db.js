6fa0e4746f7fd05e812936d2c2d29bfa2885c97c
Felipe H Murakami <fhmurakami@gmail.com>
2020-02-09 17:33:41 -0300

    Enum

--- cap2/schema-query/projeto/data/db.js
+++ cap2/schema-query/projeto/data/db.js
@@ -3,19 +3,22 @@ const usuarios = [{
   nome: 'João Silva',
   email: 'jsilva@zemail.com',
   idade: 29,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'BLOQUEADO'
 }, {
   id: 2,
   nome: 'Rafael Junior',
   email: 'rafajun@wemail.com',
   idade: 31,
-  perfil_id: 1
+  perfil_id: 1,
+  status: 'INATIVO'
 }, {
   id: 3,
   nome: 'Daniela Smith',
   email: 'danismi@umail.com',
   idade: 24,
-  perfil_id: 2
+  perfil_id: 2,
+  status: 'ATIVO'
 }]
 
 const perfis = [{
