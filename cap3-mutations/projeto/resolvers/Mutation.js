d522407d16fbd6d147e813056df326dbd4eeb8a6
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-02-13 09:32:27 -0300

    Add Chap 3 - Ex 1 - Using Input to Update User

--- cap3-mutations/projeto/resolvers/Mutation.js
+++ cap3-mutations/projeto/resolvers/Mutation.js
@@ -44,15 +44,14 @@ module.exports = {
     return excluidos ? excluidos[0] : null
   },
 
-  alterarUsuario(_, args) {
-    const i = usuarios
-      .findIndex(u => u.id === args.id)
+  alterarUsuario(_, { filtro, dados }) {
+    const i = indiceUsuario(filtro)
 
     if(i < 0) return null
 
     const usuario = {
       ...usuarios[i],
-      ...args
+      ...dados
     }
 
     usuarios.splice(i, 1, usuario)
