eb3f95fa9376fe52139173e02f72d7eb13c2857b
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-02-18 10:03:19 -0300

    Insert states with ID

--- cap8-banco_relacional/atualizarEstados.sql
+++ cap8-banco_relacional/atualizarEstados.sql
@@ -1,12 +1,12 @@
 UPDATE estados
 SET nome = 'Maranhão'
-WHERE sigla = 'MA'
+WHERE sigla = 'MA';
 
-SELECT nome FROM estados WHERE sigla = 'MA'
+SELECT nome FROM estados WHERE sigla = 'MA';
 
 UPDATE estados
 SET nome = 'Paraná',
     populacao = 11.32
-WHERE sigla = 'PR'
+WHERE sigla = 'PR';
 
-SELECT nome, populacao FROM estados WHERE sigla = 'PR'
+SELECT nome, populacao FROM estados WHERE sigla = 'PR';
