719fd2352c42965eda7dbc805479b5da5bbb27f7
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-02-19 09:49:38 -0300

    Select multiple tables with join

---
+++ cap8-banco_relacional/consultarComJoin.sql
@@ -0,0 +1,16 @@
+SELECT * FROM estados AS e, cidades AS c
+WHERE e.id = c.estado_id;
+
+SELECT
+    e.nome AS 'Estado',
+    c.nome AS 'Cidade',
+    regiao AS 'Região'
+FROM estados AS e, cidades AS c
+WHERE e.id = c.estado_id;
+
+SELECT
+    c.nome AS 'Cidade',
+    e.nome AS 'Estado',
+    regiao AS 'Região'
+FROM estados e
+INNER JOIN cidades c ON e.id = c.estado_id;
