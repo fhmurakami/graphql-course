2c18fbe34a5f2bdd781b6eaa07c1ef33bb24796f
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-05-22 09:46:11 -0300

    Add Chap4 - Ex1 resolution pt.1

--- cap4-banco_de_dados/desafio/resolvers/Query/usuario.js
+++ cap4-banco_de_dados/desafio/resolvers/Query/usuario.js
@@ -2,9 +2,21 @@ const db = require('../../config/db')
 
 module.exports = {
     async usuarios() {
-        // implementar
+        return db('usuarios')
     },
     async usuario(_, { filtro }) {
-        // implementar
+        if(!filtro) return null
+        const { id, email } = filtro
+        if(id) {
+            return db('usuarios')
+                .where({ id })
+                .first()
+        } else if(email) {
+            return db('usuarios')
+                .where({ email })
+                .first()
+        } else {
+            return null
+        }
     },
 }
