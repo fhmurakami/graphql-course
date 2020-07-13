b8b2603a4c01d6b7e1a3f5b4575eeec88985f0d0
Felipe Murakami <felipe.murakami@locaweb.com.br>
2020-07-13 09:55:53 -0300

    Add Chap4 - Ex1 resolution pt.4

--- cap4-banco_de_dados/desafio/resolvers/Mutation/usuario.js
+++ cap4-banco_de_dados/desafio/resolvers/Mutation/usuario.js
@@ -1,47 +1,49 @@
-const db = require('../../config/db')
-const { perfil: obterPerfil } = require('../Query/perfil')
+const db = require("../../config/db");
+const { perfil: obterPerfil } = require("../Query/perfil");
+const { usuario: obterUsuario } = require("../Query/usuario");
 
 module.exports = {
-    async novoUsuario(_, { dados }) {
-        try {
-            const idsPerfis = []
-            if(dados.perfis) {
-                for(filtro of dados.perfis) {
-                    const perfil = await obterPerfil(_, {
-                        filtro
-                    })
-                    if(perfil) idsPerfis.push(perfil.id)
-                }
-            }
-
-            delete dados.perfis
-
-            const [ id ] = await db('usuarios')
-                .insert(dados)
-            
-            for(perfil_id of idsPerfis) {
-                await db('usuarios_perfis')
-                    .insert({ perfil_id, usuario_id: id })
-            }
-
-            return db('usuarios')
-                .where({ id }).first()
-        } catch(e) {
-            throw new Error(e.sqlMessage)
+  async novoUsuario(_, { dados }) {
+    try {
+      const idsPerfis = [];
+      if (dados.perfis) {
+        for (filtro of dados.perfis) {
+          const perfil = await obterPerfil(_, {
+            filtro,
+          });
+          if (perfil) idsPerfis.push(perfil.id);
         }
-    },
-    async excluirUsuario(_, { filtro }) {
-        try {
-            
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
-    },
-    async alterarUsuario(_, { filtro, dados }) {
-        try {
-            
-        } catch(e) {
-            throw new Error(e.sqlMessage)
-        }
+      }
+
+      delete dados.perfis;
+
+      const [id] = await db("usuarios").insert(dados);
+
+      for (perfil_id of idsPerfis) {
+        await db("usuarios_perfis").insert({ perfil_id, usuario_id: id });
+      }
+
+      return db("usuarios").where({ id }).first();
+    } catch (e) {
+      throw new Error(e.sqlMessage);
     }
-}
+  },
+  async excluirUsuario(_, { filtro }) {
+    try {
+      const usuario = await obterUsuario(_, { filtro });
+      if (usuario) {
+        const { id } = usuario;
+        await db("usuarios_perfis").where({ usuario_id: id }).delete();
+        await db("usuarios").where({ id }).delete();
+      }
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+  async alterarUsuario(_, { filtro, dados }) {
+    try {
+    } catch (e) {
+      throw new Error(e.sqlMessage);
+    }
+  },
+};
